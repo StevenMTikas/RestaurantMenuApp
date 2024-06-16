6d9b93235fc0ad5efccbb979248a4710080283d3
Steve <stevenmtikas@gmail.com>
2024-06-15 21:32:53 -0400

    added admin section

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    # admin setup/config options
+    list_display = ("meal", "status")
+    list_filter = ("status", )
+    search_fields = ("meal", "description")
+
+# registers this in connection with Item class from models
+admin.site.register(Item, MenuItemAdmin)
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Item
+from .models import Item, MEAL_TYPE
 
 class MenuList(generic.ListView):
     # names for variable are set by Django
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
-    def get_context_data(self):
-        context = {"meals":["Pizza", "Pasta"], "ingredients":["things"]}
+    def get_context_data(self, **kwargs):
+        # prevents starting with empty dictionary
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
         return context
 
 class MenuItemDetail(generic.DetailView):
